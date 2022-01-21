75712f9d3ac23f82bb171903eb88e56eee8221d6
Naman Singh <55326403+mello369@users.noreply.github.com>
2022-01-21 19:06:05 +0530

    Create leaky_bucket.cpp

---
+++ cycle2/Leaky Bucket/leaky_bucket.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<time.h>
+#include<iostream>
+using namespace std;
+#define NOF_PACKETS 5
+
+
+
+int main()
+{
+    srand(time(0));
+    int packet_sz[NOF_PACKETS], i, clk, b_size, o_rate, p_sz_rm=0, p_sz, p_time, op;
+    for(i = 0; i<NOF_PACKETS; ++i)
+        packet_sz[i] = rand() % 100;
+    for(i = 0; i<NOF_PACKETS; ++i)
+        printf("\npacket[%d]:%d bytes\t", i, packet_sz[i]);
+    printf("\nEnter the Output rate:");
+    cin>>o_rate;
+    printf("Enter the Bucket Size:");
+    cin>>b_size;
+    for(i = 0; i<NOF_PACKETS; ++i)
+    {
+        if( (packet_sz[i] + p_sz_rm) > b_size)
+            if(packet_sz[i] > b_size)/*compare the packet siz with bucket size*/
+                printf("\n\nIncoming packet size (%dbytes) is Greater than bucket capacity (%dbytes)-PACKET REJECTED", packet_sz[i], b_size);
+            else
+                printf("\n\nBucket capacity exceeded-PACKETS REJECTED!!");
+        else
+        {
+            p_sz_rm += packet_sz[i];
+            printf("\n\nIncoming Packet size: %d", packet_sz[i]);
+            printf("\nBytes remaining to Transmit: %d", p_sz_rm);
+	while(p_sz_rm>0)
+	{
+                sleep(1);
+                if(p_sz_rm)
+                {
+                    if(p_sz_rm <= o_rate)/*packet size remaining comparing with output rate*/
+                        op = p_sz_rm, p_sz_rm = 0;
+                    else
+                        op = o_rate, p_sz_rm -= o_rate;
+                    printf("\nPacket of size %d Transmitted", op);
+                    printf("----Bytes Remaining to Transmit: %d", p_sz_rm);
+                }
+                else
+                {
+                     printf("\nNo packets to transmit!!");
+                }
+            }
+        }
+    }
+}
